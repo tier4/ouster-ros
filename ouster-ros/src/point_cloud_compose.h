9aad8decaef20c9f0418e127de0ad97a7fe9c727
Azumi Suzuki <azumi.suzuki@tier4.jp>
2026-01-29 15:40:20 +0900

    Revert "update"
    This reverts commit 3e9baaef3b06150212a1253a6308aa8f141bb4b5.

--- ouster-ros/src/point_cloud_compose.h
+++ ouster-ros/src/point_cloud_compose.h
@@ -132,9 +132,6 @@ void scan_to_cloud_f(ouster_ros::Cloud<PointT>& cloud, PointS& staging_point,
     if (!organized) cloud.clear();
     cloud.is_dense = true;
 
-    static const auto range_flat =
-        ls.field<uint32_t>(sensor::ChanField::RANGE).reshaped().eval();
-
     for (auto u = 0; u < ls.h; u += rows_step) {
         for (auto v = 0; v < ls.w; ++v) {   // TODO[UN]: consider cols_step in future
             const auto v_shift =
@@ -209,7 +206,12 @@ void scan_to_cloud_f(ouster_ros::Cloud<PointT>& cloud, PointS& staging_point,
 
             // range in meters (computed from xyz)
             if constexpr (point::has_range_v<PointT>) {
-                cloud.points[tgt_idx].range = (range_flat(src_idx)) * 0.001f;
+                const auto& xyz_tuple = cloud.points[tgt_idx];
+                const float x = xyz_tuple.x;
+                const float y = xyz_tuple.y;
+                const float z = xyz_tuple.z;
+                cloud.points[tgt_idx].range =
+                    std::sqrt(x * x + y * y + z * z);
             }
 
             // return_type (0 for first return, 1 for second, etc.)
